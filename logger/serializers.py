637610b91add6d96103ac204b0d59d889ff2a9da
ranael-garem <ranaelgarem781994@gmail.com>
2016-02-22 14:25:23 +0200

    added serializer TrackerSerializer

--- logger/serializers.py
+++ logger/serializers.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from . import models
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username')
+
+
+class TrackerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Tracker
+        fields = ('url', 'title', 'snippet')
