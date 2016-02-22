f571b7f714e889d8eb79384f81c54987ca5e8587
ranael-garem <ranaelgarem781994@gmail.com>
2016-02-22 20:34:51 +0200

    changed tracker queryset to return only trackers that belong to current authenticated user

--- logger/models.py
+++ logger/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Tracker(models.Model):
-    """ Represents an instance of a Tracker """
+    """
+    Represents an instance of a Tracker
+    """
     user = models.ForeignKey(User, related_name="trackers")
     title = models.CharField(max_length=256)
     snippet = models.CharField(max_length=256, default="JS Snippet")
--- logger/views.py
+++ logger/views.py
@@ -6,14 +6,32 @@ from .models import Tracker
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = permissions.IsCurrentUser,
 
 
 class TrackerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows current user's trackers to be viewed or edited.
+    """
     queryset = Tracker.objects.all()
     serializer_class = TrackerSerializer
 
+    def get_queryset(self):
+        """
+        returns User's trackers for authenticated users
+        """
+        if self.request.user.is_authenticated():
+            return Tracker.objects.filter(user=self.request.user)
+        else:
+            return None
+
     def perform_create(self, serializer):
+        """
+        Assigns current user to Tracker instance
+        """
         return serializer.save(user=self.request.user)
