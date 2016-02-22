6c3c5e7371bdfa8aaa8dacee89954bcc3791db7e
ranael-garem <ranaelgarem781994@gmail.com>
2016-02-22 14:24:51 +0200

    added Model Tracker

--- logger/models.py
+++ logger/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tracker(models.Model):
+    """ Represents an instance of a Tracker """
+    user = models.ForeignKey(User, related_name="trackers")
+    title = models.CharField(max_length=256)
+    snippet = models.CharField(max_length=256, default="JS Snippet")
