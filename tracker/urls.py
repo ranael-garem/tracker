596b1a5a29266d2baadc9dae1e2004045b4433dc
ranael-garem <ranaelgarem781994@gmail.com>
2016-02-23 13:26:26 +0200

    added urls for the added Views

--- tracker/urls.py
+++ tracker/urls.py
@@ -9,8 +9,10 @@ router.register(r'trackers', views.TrackerViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^load/(?P<pk>[0-9]+)/$', views.PageLoadView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework'))
+        include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^test/$', views.UserIdView.as_view()),
 
 ]
