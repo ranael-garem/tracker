de1904e961e54d02a520c541e98c27d8ed518e02
ranael-garem <ranaelgarem781994@gmail.com>
2016-02-23 13:17:11 +0200

    Added model PageLoad

--- logger/models.py
+++ logger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from uuidfield import UUIDField
 
 
 class Tracker(models.Model):
@@ -9,3 +10,13 @@ class Tracker(models.Model):
     user = models.ForeignKey(User, related_name="trackers")
     title = models.CharField(max_length=256)
     snippet = models.CharField(max_length=256, default="JS Snippet")
+
+
+class PageLoad(models.Model):
+    """
+    Represents the Loads of a page by a single user
+    saved in a session
+    """
+    tracker = models.ForeignKey(Tracker)
+    user_id = UUIDField(auto=True, unique=True)
+    loads = models.IntegerField(default=1)
