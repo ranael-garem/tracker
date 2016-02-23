5dd8863a4ca261110308bd47a2b93d036b842717
ranael-garem <ranaelgarem781994@gmail.com>
2016-02-23 14:00:56 +0200

    added method to Tracker that return total page loads

--- logger/models.py
+++ logger/models.py
@@ -11,12 +11,18 @@ class Tracker(models.Model):
     title = models.CharField(max_length=256)
     snippet = models.CharField(max_length=256, default="JS Snippet")
 
+    def total_page_loads(self):
+        """
+        returns number of page loads for a Tracker
+        """
+        return self.page_loads.aggregate(models.Sum('loads'))["loads__sum"]
+
 
 class PageLoad(models.Model):
     """
     Represents the Loads of a page by a single user
     saved in a session
     """
-    tracker = models.ForeignKey(Tracker)
+    tracker = models.ForeignKey(Tracker, related_name="page_loads")
     user_id = UUIDField(auto=True, unique=True)
     loads = models.IntegerField(default=1)
