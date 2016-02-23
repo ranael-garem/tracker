f1e43070c95161de83ca93aaf82fa63c8d6f2a5c
ranael-garem <ranaelgarem781994@gmail.com>
2016-02-23 14:01:37 +0200

    added PageLoad Serializer and added total_page_loads to TrackerSerializer

--- logger/serializers.py
+++ logger/serializers.py
@@ -9,9 +9,17 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'username')
 
 
+class PageLoadSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.PageLoad
+        fields = ('id', 'user_id', 'loads')
+
+
 class TrackerSerializer(serializers.HyperlinkedModelSerializer):
+    # page_loads = PageLoadSerializer(many=True)
     snippet = serializers.CharField(read_only=True)
 
     class Meta:
         model = models.Tracker
-        fields = ('url', 'title', 'snippet')
+        fields = ('url', 'title', 'snippet', 'total_page_loads')
