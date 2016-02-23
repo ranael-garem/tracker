8ec32dd1ed9dbfddd2129c525fe658f0096d3fc9
ranael-garem <ranaelgarem781994@gmail.com>
2016-02-23 13:23:18 +0200

    Added PageLoadView

--- logger/views.py
+++ logger/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import UserSerializer, TrackerSerializer
 from . import permissions
-from .models import Tracker
+from .models import Tracker, PageLoad
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -36,3 +38,26 @@ class TrackerViewSet(viewsets.ModelViewSet):
         Assigns current user to Tracker instance
         """
         return serializer.save(user=self.request.user)
+
+
+class PageLoadView(APIView):
+    """
+    Creates or increments 'loads' of PageLoad Object
+    for the current user saved in the SESSION
+    """
+
+    def get(self, request, pk, format=None):
+        if 'user_id' in request.session:
+            user_id = request.session['user_id']
+            (page_load, created) = PageLoad.objects.get_or_create(
+                user_id=user_id)
+            if not created:
+                page_load.loads += 1
+                page_load.save()
+            print PageLoad.objects.get(user_id=user_id).loads
+        else:
+            page_load = PageLoad.objects.create(tracker_id=pk)
+            request.session['user_id'] = page_load.user_id
+
+        return Response({'user_id': request.session['user_id'],
+                        'loads': page_load.loads})
