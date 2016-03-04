cd748d349e4b16b068f98a1f1255022d057e74d0
ranael-garem <ranaelgarem781994@gmail.com>
2016-03-04 12:53:01 +0200

    added LoginView and LogoutView

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
+from rest_framework import views
+from rest_framework import status
+from rest_framework import permissions
 from rest_framework.response import Response
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 from authentication.serializers import UserSerializer
 from authentication.permissions import IsCurrentUser
@@ -33,3 +37,42 @@ class UserViewSet(viewsets.ModelViewSet):
             data.update({'message': 'Password Changed Successfully'})
 
         return Response(data)
+
+
+class LoginView(views.APIView):
+    """
+    View responsible for logging in Users
+    """
+
+    def post(self, request, format=None):
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+
+        if User.objects.filter(email=email).exists():
+            username = User.objects.get(email=email).username
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    serialized = UserSerializer(
+                        user, context={'request': request})
+                    print serialized.data
+                    return Response(serialized.data)
+                else:
+                    return Response({
+                        'message': 'This account has been disabled.'
+                    }, status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response({
+            'message': 'Email or Password is incorrect'
+        }, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        logout(request)
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
