77f4dbc849b62adb70f7086ee544c8434c593170
ranael-garem <ranaelgarem781994@gmail.com>
2016-03-04 23:00:35 +0200

    added services for get, update, delete tracker

--- static/javascripts/logger/services/logger.service.js
+++ static/javascripts/logger/services/logger.service.js
@@ -13,6 +13,9 @@
     var Trackers = {
       all: all,
       create_tracker: create_tracker,
+      get: get,
+      update: update,
+      destroy: destroy,
 
     };
 
@@ -35,6 +38,30 @@
       function errorFn(data, status, headers, config) {
         return data.data;
       }
+    }
+
+    function destroy(tracker) {
+      return $http.delete('/api/trackers/' + tracker.id + '/');
+    }
+
+
+
+    function get(tracker_id) {
+      return $http.get('/api/trackers/' + tracker_id + '/').then(successFn, errorFn);
+
+
+      function successFn(data, status, headers, config) {
+        return data.data;
+      }
+
+      function errorFn(data, status, headers, config) {
+        return data.data;
+      }
+    }
+
+
+    function update(tracker) {
+      return $http.put('/api/trackers/' + tracker.id + '/', tracker);
     }
 
   }
