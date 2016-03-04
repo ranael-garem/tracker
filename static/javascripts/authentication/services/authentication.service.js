b0241ec5aa61aff450a2099394a8cd716144987e
ranael-garem <ranaelgarem781994@gmail.com>
2016-03-04 12:50:46 +0200

    added service for login and logout

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -11,17 +11,64 @@
   function Authentication($http) {
 
     var Authentication = {
-      register: register
+      login: login,
+      register: register,
+      logout: logout,
     };
 
     return Authentication;
 
-    function register(email, password, username) {
-      return $http.post('/api/users/', {
-        username: username,
-        password: password,
-        email: email
-      });
+  function register(email, password, username) {
+    alert(email);
+    return $http.post('/api/users/', {
+      username: username,
+      password: password,
+      email: email
+    }).then(regSuccess, regError);
+
+
+    function regSuccess(data, status, headers, config) {
+      Authentication.login(email, password);
+      // window.location = '/';
     }
+
+    function regError(data, status, headers, config) {
+      console.error(data);
+    }
+  }
+
+  function login(email, password) {
+    return $http.post('/api/auth/login/', {
+      email: email, password: password
+    }).then(loginSuccess, loginError);
+
+
+    function loginSuccess(data, status, headers, config) {
+
+      window.location = '/';
+    }
+
+    function loginError(data, status, headers, config) {
+      return data['data']['message'];
+    }
+  }
+
+
+
+  function logout() {
+    return $http.post('/api/auth/logout/')
+        .then(logoutSuccess, logoutError);
+
+      function logoutSuccess(data, status, headers, config) {
+        // Authentication.unauthenticate();
+
+        window.location = '/';
+      }
+
+      function logoutError(data, status, headers, config) {
+        console.error(data);
+      }
+    }
+
   }
 })();
