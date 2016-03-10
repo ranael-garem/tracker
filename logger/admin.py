901bc434442991c50633a820738105a65de88df1
ranael-garem <ranaelgarem781994@gmail.com>
2016-03-10 11:34:53 +0200

    registered models in admin.py

--- logger/admin.py
+++ logger/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from logger.models import PageLoad, MouseClick, Tracker, TrackedUser
 
 # Register your models here.
+admin.site.register(Tracker)
+admin.site.register(PageLoad)
+admin.site.register(MouseClick)
+admin.site.register(TrackedUser)
