1026704f67b32774de5b2f040063245faff8b2b4
ranael-garem <ranaelgarem781994@gmail.com>
2016-03-14 13:19:13 +0200

    added service for new_users

--- static/javascripts/logger/services/dashboard.service.js
+++ static/javascripts/logger/services/dashboard.service.js
@@ -13,6 +13,8 @@
     var Dashboard = {
       visits_over_time: visits_over_time,
       visits_with_date: visits_with_date,
+      new_users: new_users,
+      new_users_with_date: new_users_with_date,
     };
 
     return Dashboard;
@@ -40,6 +42,41 @@
       var to_date_string = to_date.getFullYear() + '/' + (to_date.getMonth() + 1) + '/' + to_date.getDate() + '/';
 
       return $http.get('/visits/' + tracker_id + '/' + from_date_string + to_date_string).then(successFn, errorFn);
+
+
+      function successFn(data, status, headers, config) {
+        return data.data;
+      }
+
+      function errorFn(data, status, headers, config) {
+        return data.data;
+      }
+    }
+
+
+    function new_users(tracker_id) {
+      return $http.get('/new-users/' + tracker_id + '/').then(successFn, errorFn);
+
+
+      function successFn(data, status, headers, config) {
+        return data.data;
+      }
+
+      function errorFn(data, status, headers, config) {
+        return data.data;
+      }
+    }
+
+
+    function new_users_with_date(tracker_id, date){
+      console.log(date)
+      var from_date = new Date(date[0]);
+      var to_date = new Date(date[1]);
+
+      var from_date_string = from_date.getFullYear() + '/' + (from_date.getMonth() + 1) + '/' + from_date.getDate() + '/';
+      var to_date_string = to_date.getFullYear() + '/' + (to_date.getMonth() + 1) + '/' + to_date.getDate() + '/';
+
+      return $http.get('/new-users/' + tracker_id + '/' + from_date_string + to_date_string).then(successFn, errorFn);
 
 
       function successFn(data, status, headers, config) {
