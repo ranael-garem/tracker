d261522db8cd3c4ce4c89c0cad71e8180fd877f5
ranael-garem <ranaelgarem781994@gmail.com>
2016-03-14 23:19:06 +0200

    added urls for bounce rate

--- reports/urls.py
+++ reports/urls.py
@@ -24,4 +24,9 @@ urlpatterns = [
         reports_views.NewUsersView.as_view()),
     url(r'^new-users/(?P<pk>[0-9]+)/(?P<FY>\d{4})/(?P<FM>\d+)/(?P<FD>\d+)/(?P<TY>\d{4})/(?P<TM>\d+)/(?P<TD>\d+)/$',
         reports_views.NewUsersView.as_view()),
+
+    url(r'^bounce/(?P<pk>[0-9]+)/$',
+        reports_views.BounceRateView.as_view()),
+    url(r'^bounce/(?P<pk>[0-9]+)/(?P<FY>\d{4})/(?P<FM>\d+)/(?P<FD>\d+)/(?P<TY>\d{4})/(?P<TM>\d+)/(?P<TD>\d+)/$',
+        reports_views.BounceRateView.as_view()),
 ]
