5592193cf8df25c283213d2d4fc3835f0a5d560a
ranael-garem <ranaelgarem781994@gmail.com>
2016-03-14 10:53:57 +0200

    added Dashboard Factory

---
+++ static/javascripts/logger/services/dashboard.service.js
@@ -0,0 +1,56 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('tracker.logger.services')
+    .factory('Dashboard', Dashboard);
+
+  Dashboard.$inject = ['$http'];
+
+
+  function Dashboard($http) {
+
+    var Dashboard = {
+      visits_over_time: visits_over_time,
+      visits_with_date: visits_with_date,
+    };
+
+    return Dashboard;
+
+    function visits_over_time(tracker_id) {
+      return $http.get('/visits/' + tracker_id + '/').then(successFn, errorFn);
+
+
+      function successFn(data, status, headers, config) {
+        return data.data;
+      }
+
+      function errorFn(data, status, headers, config) {
+        return data.data;
+      }
+    }
+
+
+    function visits_with_date(tracker_id, date){
+      console.log(date)
+      var from_date = new Date(date[0]);
+      var to_date = new Date(date[1]);
+
+      var from_date_string = from_date.getFullYear() + '/' + (from_date.getMonth() + 1) + '/' + from_date.getDate() + '/';
+      var to_date_string = to_date.getFullYear() + '/' + (to_date.getMonth() + 1) + '/' + to_date.getDate() + '/';
+
+      return $http.get('/visits/' + tracker_id + '/' + from_date_string + to_date_string).then(successFn, errorFn);
+
+
+      function successFn(data, status, headers, config) {
+        return data.data;
+      }
+
+      function errorFn(data, status, headers, config) {
+        return data.data;
+      }
+    }
+
+  }
+
+})();
