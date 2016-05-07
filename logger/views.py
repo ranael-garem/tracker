1eec14bde43a89e6b06ce7fc01764dc81cb0a85c
ranael-garem <ranaelgarem781994@gmail.com>
2016-05-08 00:34:27 +0200

    correclty formated page_path

--- logger/views.py
+++ logger/views.py
@@ -140,6 +140,10 @@ class PageLoadView(APIView):
 
         if not href.startswith('http://'):
             href = href[:6] + '/' + href[6:]
+        if path == 'x':
+            path = '/'
+        else:
+            path = path[1:]
         (page, created) = Page.objects.get_or_create(
             path_name=path, tracker_id=pk, href=href)
         if page.height != height:
