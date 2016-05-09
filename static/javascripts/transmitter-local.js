905c883d3d7ec5c9d0b21d9031fd96bfe3440447
ranael-garem <ranaelgarem781994@gmail.com>
2016-05-09 11:32:53 +0200

    Scroll Heat Map Working Locally

--- static/javascripts/transmitter-local.js
+++ static/javascripts/transmitter-local.js
@@ -1,21 +1,21 @@
-var tracker_id;
+var tracker_id = 1;
 var scripts = document.getElementsByTagName('script');
-for (i = 0; i < scripts.length; i++) {
-    if (scripts[i].src.indexOf('http://127.0.0.1:8000/static/javascripts/transmitter.js') > -1) {
-        tracker_id = scripts[i].src.replace(/^[^\?]+\??/, '').split('=')[1];
-        break;
-    }
-}
-// <!-- Remove this when deployed 
-if (tracker_id == undefined){
-    tracker_id = 1;
-}
-//-->
+// for (i = 0; i < scripts.length; i++) {
+//     if (scripts[i].src.indexOf('http://127.0.0.1:8000/static/javascripts/transmitter.js') > -1) {
+//         tracker_id = scripts[i].src.replace(/^[^\?]+\??/, '').split('=')[1];
+//         break;
+//     }
+// }
+// // <!-- Remove this when deployed 
+// if (tracker_id == undefined){
+//     tracker_id = 29;
+// }
+// //-->
 
-var title = document.title
-var url = window.location.href
-var pathname = window.location.pathname
-
+var title = document.title;
+var href = window.location.href;
+var pathname = window.location.pathname;
+var page_load_id = 0;
 var max_scroll_height = 0;
 var mouse_moves = [];
 var invoke = false;
@@ -139,34 +139,88 @@ function screenShot() {
     }, 2000);
 }
 
-function sendMouseMoves() {
-    var movesToSend = mouse_moves.slice(1, 10);
-    mouse_moves = [];
-    Client.post('http://127.0.0.1:8000/mouse/move/' + tracker_id + '/' + pathname, movesToSend, function(response) {
-
+function heatMapCanvas(clicks, body) {
+    var heatmap = h337.create({
+        container: body,
+        maxOpacity: .6,
+        radius: 20,
+        blur: .90,
+        // backgroundColor with alpha so you can see through it
+        backgroundColor: 'rgba(0, 0, 58, 0)'
     });
+    var dict = [];
+    var x = 0;
+    for (x in clicks) {
+        dict.push({ x: clicks[x][1], y: clicks[x][0], value: 100 })
+    }
+    var data = {
+        max: 10000,
+        min: 0,
+        data: dict,
+    };
+    heatmap.setData(data);
+    heatmap.repaint();
+    if (window.location.href.indexOf("heatmapxy") == -1) {
+        window.location.hash = window.location.hash + 'y';
+        window.location.reload();
+    }
+}
+
+function mouseClickHeatMapDeployed() {
+    var body = document.getElementsByTagName("BODY")[0];
+
+    $.ajax({
+        url: 'http://127.0.0.1:8000/reports/heatmap/' + pathname + '/tracker_id' + '/' + tracker_id + '/',
+        dataType: 'jsonp',
+        success: function(response) {
+            console.log(response);
+            heatMapCanvas(response.clicks, body);
+        },
+        error: function(error) {
+            console.log(error);
+            heatMapCanvas(JSON.parse(error.responseText).clicks, body);
+        }
+    });
+}
+
+function sendMouseMoves() {
+    var movesToSend = mouse_moves;
+    mouse_moves = [];
+    // Client.post('http://127.0.0.1:8000/mouse/move/' + tracker_id + '/' + pathname, movesToSend, function(response) {
+
+    // });
+    var img = document.createElement("img");
+    img.src = 'http://127.0.0.1:8000/mouse/move/' + tracker_id + '/pathname/x' + pathname + '/href/' + window.location.href + '/coordinates/' + movesToSend;
+    img.width = 1;
+    img.height = 1;
+    var html = document.getElementsByTagName("HTML")[0];
+    html.appendChild(img);
 }
 
 document.onreadystatechange = function() {
     Client = new HttpClient();
     // console.log(document.referrer);
-    if (window.location.href.indexOf("heatmapx") > -1) {
-        mouseClickHeatMap();
+    if (document.readyState == "complete" && window.location.href.indexOf("heatmapx") > -1) {
+        mouseClickHeatMapDeployed();
     } else if (document.readyState == "complete" && document.referrer.indexOf("/reports/scroll/heatmap/") > -1) {
-        screenShot();
+        // screenShot();
 
     } else if (document.readyState == "complete") {
         // screenShot();
         Client.get('http://ip-api.com/json/?fields=country,countryCode,regionName,city,query', function(response) {
-            // console.log(navigator);
             var data = JSON.parse(response);
             var demographics = '/demographics/' + data.country + '/' + data.countryCode + '/' + data.city + '/' + data.regionName + '/' + data.query;
-            var _docHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+            var body = document.body,
+                html = document.documentElement;
 
-            Client.get('http://127.0.0.1:8000/load/' + tracker_id + '/' + _docHeight + '/' + pathname + demographics + '/' + navigator.language, function(response) {
+            var height = Math.max(body.scrollHeight, body.offsetHeight,
+                html.clientHeight, html.scrollHeight, html.offsetHeight);
+            Client.get('http://127.0.0.1:8000/load/' + tracker_id + '/' + height + '/pathname/x' + pathname + demographics + '/' + navigator.language + '/href/' + window.location.href, function(response) {
                 console.log(response);
+                page_load_id = JSON.parse(response).page_load;
             });
         })
+
 
 
     }
@@ -176,24 +230,37 @@ window.onscroll = function() {
     scroll_height = document.documentElement.scrollTop || document.body.scrollTop;
     if (scroll_height > max_scroll_height) {
         max_scroll_height = scroll_height;
-        // Client.get('http://127.0.0.1:8000/api/scroll/' + max_scroll_height + '/', function(response) {});
+        Client.get('http://127.0.0.1:8000/api/scroll/' + max_scroll_height + '/', function(response) {});
     }
 }
 
+// window.onclick = function(e) {
+//     sendMouseMoves();
+//     var x = e.layerX;
+//     var y = e.layerY;
+//     if (e.touches) {
+//         x = e.touches[0].pageX;
+//         y = e.touches[0].pageY;
+//     }
+//     console.log(x,y);
+//     // Client = new HttpClient();
+//     // Client.get('http://127.0.0.1:8000/click/' + tracker_id + '/' + x + '/' + y + '/' + pathname, function(response) {
+
+//     // });
+//     var img = document.createElement("img");
+//     img.src = 'http://127.0.0.1:8000/click/' + tracker_id + '/' + x + '/' + y  + '/pathname/x' + pathname + '/href/' + window.location.href;
+//     img.width = 1;
+//     img.height = 1;
+//     var html = document.getElementsByTagName("HTML")[0];
+//     html.appendChild(img);
+// }
 window.onclick = function(e) {
     sendMouseMoves();
-    var x = e.layerX;
-    var y = e.layerY;
-    if (e.touches) {
-        x = e.touches[0].pageX;
-        y = e.touches[0].pageY;
-    }
-    // Client = new HttpClient();
-    // Client.get('http://127.0.0.1:8000/click/' + tracker_id + '/' + x + '/' + y + '/' + pathname, function(response) {
-
-    // });
+    var coordinates = findDocumentCoords(e);
+    x = coordinates[0];
+    y = coordinates[1];
     var img = document.createElement("img");
-    img.src = 'http://127.0.0.1:8000/click/' + tracker_id + '/' + x + '/' + y + '/' + pathname;
+    img.src = 'http://127.0.0.1:8000/click/' + tracker_id + '/' + x + '/' + y + '/pathname/x' + pathname + '/href/' + window.location.href;
     img.width = 1;
     img.height = 1;
     var html = document.getElementsByTagName("HTML")[0];
@@ -206,13 +273,30 @@ window.onbeforeunload = function(e) {
 };
 
 window.onmousemove = function(e) {
-    if (mouse_moves.length == 100) {
+    if (mouse_moves.length == 200) {
         sendMouseMoves();
     }
     if (!invoke) {
         return;
     }
-    var x = e.layerX;
-    var y = e.layerY;
+    var coordinates = findDocumentCoords(e);
+    x = coordinates[0];
+    y = coordinates[1];
     mouse_moves.push(x + ',' + y);
+}
+
+function findDocumentCoords(mouseEvent) {
+    var xpos
+    var ypos;
+    if (mouseEvent) {
+        //FireFox
+        xpos = mouseEvent.pageX;
+        ypos = mouseEvent.pageY;
+    } else {
+        //IE
+        xpos = window.event.x + document.body.scrollLeft - 2;
+        ypos = window.event.y + document.body.scrollTop - 2;
+    }
+
+    return [xpos, ypos]
 }
